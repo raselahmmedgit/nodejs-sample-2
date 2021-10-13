4e2ca48a3626e17267b2fb52f0b7186c71ef3e59
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2021-10-13 15:10:59 +0600

    2021-10-13-2

---
+++ nodejsexpressjwt/db-script/nodejs_db.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 13, 2021 at 11:07 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nodejs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `Id` varchar(256) NOT NULL,
+  `RoleName` varchar(256) NOT NULL,
+  `NormalizedRoleName` varchar(256) NOT NULL,
+  `IsActive` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `StudentId` int(11) NOT NULL,
+  `StudentName` varchar(256) DEFAULT NULL,
+  `EmailAddress` varchar(256) DEFAULT NULL,
+  `DateOfBirth` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`StudentId`, `StudentName`, `EmailAddress`, `DateOfBirth`) VALUES
+(1, 'john', 'john@mail.com', '2021-09-16 16:17:17'),
+(2, 'rasel', 'rasel@mail.com', '1985-10-01 10:58:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userroles`
+--
+
+CREATE TABLE `userroles` (
+  `UserId` varchar(256) NOT NULL,
+  `RoleId` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Id` varchar(256) NOT NULL,
+  `UserName` varchar(256) NOT NULL,
+  `NormalizedUserName` varchar(256) NOT NULL,
+  `Email` varchar(256) NOT NULL,
+  `NormalizedEmail` varchar(256) NOT NULL,
+  `EmailConfirmed` tinyint(1) DEFAULT NULL,
+  `PasswordHash` varchar(256) NOT NULL,
+  `IsActive` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`StudentId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `StudentId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
